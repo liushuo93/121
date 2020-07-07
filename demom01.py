c1207a200e424db0ed75097d0212a7ac66a3ee9f
liushuo93 <1695908508@qq.com>
2020-07-07 16:48:59 +0800

    diyicixiang

---
+++ demom01.py
@@ -0,0 +1,29 @@
+'''
+print("hello world!")#字符串
+print(True)#整数
+print("哈哈哈",13132)
+print("hh"+"谢谢")
+print("hh"*45)
+print(3*5+6)
+print(2>3)
+print("zhangsan")
+q="hh"
+print(q)
+a=input()
+print("获取的内容：",a)
+b=input("qinshuru:")
+print(b)
+a=input("请输入a：")
+b=input("请输入b：")
+print("结果：",a+b)
+'''
+# c=type(312321)
+# print(c)
+# print(type("12313"))
+# a=int("239123692")
+# print(type(a))
+a=input("请输入a：")
+b=input("请输入b：")
+c=len(a)
+d=len(b)
+print("结果：",c+d)
