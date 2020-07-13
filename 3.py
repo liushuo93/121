b21170129b71ee453c25e5dfecd4db1bda459b75
liushuo93 <1695908508@qq.com>
2020-07-13 21:14:12 +0800

    第四天

--- 3.py
+++ 3.py
@@ -1,1 +1,9 @@
-print('hello world')
+# a=input('name:')
+# b=input('age:')
+# c=input('sex:')
+userinfo={}
+userinfo.update(name=input('name:'))
+userinfo.update(age=input('age:'))
+userinfo.update(sax=input('sex:'))
+
+print(userinfo)
