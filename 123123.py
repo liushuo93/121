be5386a4be15d57318eff409a36f06bb8a8181a4
liushuo93 <1695908508@qq.com>
2020-07-13 21:03:19 +0800

    diwut

--- 123123.py
+++ 123123.py
@@ -14,18 +14,18 @@
 # a.append("dhsjadhkashdk")
 
 a.remove(4342)
-# print(a)
-# a={"name":"张三",0:'232',321:'qwew'}
-# print(a['name'])
-# a['high']=180
-# print(a)
+print(a)
+a={"name":"张三",0:'232',321:'qwew'}
+print(a['name'])
+a['high']=180
+print(a)
 # b=a.get('name')
 # print(b)
 # c=a.pop(0)
-# print(c)
-# print(a)
-# a.update(name=3123213)
-# print(a)
+print(c)
+print(a)
+a.update(name=3123213)
+print(a)
 # a.update(na234me=3123213)
 # print(a)
 a=input('name:')
