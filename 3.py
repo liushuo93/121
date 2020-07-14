1a5a7371d5655c024beb26e29aff0a6968f48622
liushuo93 <1695908508@qq.com>
2020-07-14 09:59:55 +0800

    714

--- 3.py
+++ 3.py
@@ -7,3 +7,57 @@
 # userinfo.update(sax=input('sex:'))
 
 # print(userinfo)
+#判断
+# a=12
+# b=2
+# if a>b:
+#     print('a比b大')
+#     print('a比b大')
+# else:
+#     print('a比b小')
+# if a==12:
+#     print('31213')
+# age=int(input('请输入你的年薪：'))
+# if a>30:
+#     print('贪心')
+# elif a>20:
+#     print('小康生活')
+# elif a>10:
+#     print('满足温饱')
+# else:
+#     print('废物')
+# if age<=10:
+#     print("废物")
+# elif age>10 and age<=20:
+#     print('满足温饱')
+# elif age>20 and age<=30:
+#     print('小康生活')
+# else:
+#     print('贪心')
+# a="你好"
+# if a in '你好hdkjkdjsk':
+#     print('ok')
+# else:
+#     print('buhao')
+
+
+
+# a=input('请输入:')
+# if a=='':
+#     print('不能为空')
+#     exit()
+# if type(a) is int:
+#     a=int(a)
+# else:
+#     print('请输入正确的年龄')
+#     exit()
+# if a>10:
+#     print('da')
+# else:
+#     print('xiao')
+
+
+
+
+
+    
